74b888fb444716f18a8908f5cbf2bdd33bd7b122
yashicakrish <yashicakrishhh@gmail.com>
2025-12-12 01:09:44 +0000

    i did my python code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+# Name:Yashica Krishnaperumal 
+
+def add(a, b):
+    return a + b;     
+
+def subtract(a, b):
+    return a - b;  
+
+print("************************************")
+print("|                                  |")
+print("| Welcome to Addition or Subtraction|")
+print("|                                  |")
+print("************************************")
+type = input("Do you want to do math?(add/subtract) ")
+if type == "add" or type == "subtract":
+    a = int(input("Enter First Number "))
+    b = int(input("Enter second Number "))
+    if type == "add":
+        res = add(a, b)
+        print(" Add : Result : ", res)
+    else:
+        res = subtract(a, b)
+        print(" Subtract : Result : ", res)      
+else:
+   print("Sorry, Invalid input")   
 
